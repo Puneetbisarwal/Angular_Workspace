ec8c99a70809000bd0603182dd772eb401975780
Puneetbisarwal <puneetbisarwal@gmail.com>
2024-08-18 22:38:13 +0530

    My 4th commit Video Part 87 Using Signals in Services

--- Section_4_Investment_Calculator/src/app/investment-results/investment-results.component.ts
+++ Section_4_Investment_Calculator/src/app/investment-results/investment-results.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { CurrencyPipe } from '@angular/common';
 
 import { InvestmentService } from '../investment.service';
@@ -14,8 +14,7 @@ export class InvestmentResultsComponent {
   private investmentService = inject(InvestmentService);
 
 
-  get results() {
-    return this.investmentService.resultData;
-  } 
+  results = computed(() => this.investmentService.resultData());
+  // results = this.investmentService.resultData.asReadonly();
   
 }
