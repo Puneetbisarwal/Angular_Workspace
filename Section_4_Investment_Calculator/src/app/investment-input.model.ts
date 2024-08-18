c33ade866b2fbe42f3f189d5ecb53d0e75f49fdd
Puneetbisarwal <puneetbisarwal@gmail.com>
2024-08-18 20:42:31 +0530

    My 2nd commit Video Part 84 Formatting Output with a Pipe

---
+++ Section_4_Investment_Calculator/src/app/investment-input.model.ts
@@ -0,0 +1,21 @@
+
+  export interface InvestmentInput {
+    initialInvestment: number; 
+    duration: number;
+    expectedReturn: number; 
+    annualInvestment: number;
+  }
+
+
+//   export type InvestmentInput {
+//     initialInvestment: number; 
+//     duration: number;
+//     expectedReturn: number; 
+//     annualInvestment: number;
+//   }
+
+
+      
+      
+  
+  
