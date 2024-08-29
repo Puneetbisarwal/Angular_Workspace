c83f53489b8dc8dd884224e22e43ca6b67d1a8ba
Puneetbisarwal <puneetbisarwal@gmail.com>
2024-08-29 20:57:28 +0530

    My 12th commit Video Part 120 Manipulating State & Using Literal Values

--- Section_6_Components_Templates/src/app/dashboard/server-status/server-status.component.ts
+++ Section_6_Components_Templates/src/app/dashboard/server-status/server-status.component.ts
@@ -8,7 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './server-status.component.css'
 })
 export class ServerStatusComponent {
+  currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
 
-  currentStatus = 'offline';
+  constructor() {
+    setInterval(() => {
+      const rnd = Math.random(); // 0 - 0.99999999999999
+
+      if (rnd < 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+
+  }
 
 }
